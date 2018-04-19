970bbd91505d8b9b6792f17a6a1d9485765fd6cb
Sabin Raj <sawbean.raz@googlemail.com>
2018-04-19 19:43:51 +0100

    Add: Pluralizer

--- Aster.Framework/Localization/Localizer.cs
+++ Aster.Framework/Localization/Localizer.cs
@@ -5,4 +5,5 @@ using System.Text;
 namespace Aster.Framework.Localization
 {
     public delegate LocalizedString Localizer(string text, params object[] args);
+    public delegate LocalizedString Pluralizer(string singular, string plural, int count, params object[] args);
 }
--- Aster.Framework/Mvc/Razor/AsterRazorPage.cs
+++ Aster.Framework/Mvc/Razor/AsterRazorPage.cs
@@ -1,6 +1,7 @@
 ﻿using Aster.Framework.Localization;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 
@@ -11,6 +12,7 @@ namespace Aster.Framework.Mvc.Razor {
     public abstract class AsterRazorPage<TModel>: Microsoft.AspNetCore.Mvc.Razor.RazorPage<TModel> {
 
         private Localizer _localizer;
+        private Pluralizer _pluralizer;
 
 
         public Localizer T {
@@ -32,12 +34,30 @@ namespace Aster.Framework.Mvc.Razor {
                 if(_localizer == null) {
                     _localizer = (text, args) => {
                         return new LocalizedString($"{text} TERO TAUKO DONE");
-                    };
-                }
+                    };                    
+                }                
                 return _localizer;
             }
         }
 
+        public Pluralizer Plural {
+            get {
 
+                if(_pluralizer == null) {
+                    _pluralizer = (singular, plural, count, args) => {
+                        //TODO: 
+                        //1. If count is one get localization text for singlur else plural from service
+                        //2. Convert the text to localized string
+                        return new LocalizedString(
+                            string.Format(
+                                count == 1 ? singular : plural,
+                                new object[] { count }.Concat(args).ToArray()));
+                    };
+                }
+
+                return _pluralizer;
+            }
+
+        }
     }
 }

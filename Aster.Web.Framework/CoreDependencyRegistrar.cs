1b5218bb4292b687ba63042a431da980c6cd9345
Thang <Thang.Truong@giantgroup.com>
2018-05-17 11:22:59 +0100

    Sorted out the conflicit issue

--- Aster.Web.Framework/CoreDependencyRegistrar.cs
+++ Aster.Web.Framework/CoreDependencyRegistrar.cs
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
-﻿using Aster.Core.Services.Candidates;
-=======
-﻿using Aster.Core.Services.Contractors;
->>>>>>> 7bf30bca19e647fe74841342c59cc092554d7bd0
+
+
+using Aster.Core.Services.Candidates;
+
+
 using Aster.Core.Services.Users;
 using Aster.System.Abstractions;
 using Autofac;
@@ -20,11 +20,10 @@ namespace Aster.Web.Framework {
                 .InstancePerLifetimeScope();
 
 
-<<<<<<< HEAD
             builder.RegisterType<CandidateService>().As<ICandidateService>()
-=======
-            builder.RegisterType<ContractorService>().As<IContractorService>()
->>>>>>> 7bf30bca19e647fe74841342c59cc092554d7bd0
+
+        //    builder.RegisterType<ContractorService>().As<IContractorService>()
+
                 .InstancePerLifetimeScope();
         }
     }

53d09ddc6c8c2747467e16744432998bbb2dda1a
Sabin Raj DANGOL <sawbean.raz@googlemail.com>
2019-06-26 01:45:36 +0100

    Update contractor seed down

--- packages/database/seeders/20190625234223-contractor-seeder.js
+++ packages/database/seeders/20190625234223-contractor-seeder.js
@@ -1,11 +1,7 @@
 const FakeContractors = require('../utils/contractors-data');
 
 module.exports = {
-  up: (queryInterface) => {
-    return queryInterface.bulkInsert('contractors',
-      FakeContractors(100), {});
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('People', null, {});
-  }
+  up: queryInterface => queryInterface.bulkInsert('contractors',
+    FakeContractors(100), {}),
+  down: queryInterface => queryInterface.bulkDelete('contractors', null, {}),
 };
